7e9a88ec53a2c2b7e4322c7e2e523bb9f149442c
dgeniust <dathiichan141@gmail.com>
2026-03-24 14:54:45 +0700

    feat: add vercel.json

--- demo-rovian-shop-fe/src/pages/Landing/LandingPage.jsx
+++ demo-rovian-shop-fe/src/pages/Landing/LandingPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import HeroSection from "./HeroSection";
-import Header from "../../components/layout/Header";
 import LookbookGrid from "./LookbookGrid";
 import AboutSection from "./AboutSection";
 import ContactSection from "./ContactSection";
