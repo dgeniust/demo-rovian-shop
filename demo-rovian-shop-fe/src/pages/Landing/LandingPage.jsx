31fe4293f723cf05adb80d57b85d374764dc3158
dgeniust <dathiichan141@gmail.com>
2026-04-02 13:37:36 +0700

    feat: seo

--- demo-rovian-shop-fe/src/pages/Landing/LandingPage.jsx
+++ demo-rovian-shop-fe/src/pages/Landing/LandingPage.jsx
@@ -4,22 +4,43 @@ import LookbookGrid from "./LookbookGrid";
 import AboutSection from "./AboutSection";
 import ContactSection from "./ContactSection";
 import FeaturedProducts from "./FeaturedProducts";
-
+import { Helmet } from "react-helmet-async";
 const LandingPage = () => {
   return (
-    <div className="min-h-screen bg-black text-white font-['Google_Sans_Flex']">
-      {/* HERO SECTION (LOOKBOOK) */}
-      <HeroSection />
+    <>
+      <Helmet>
+        {/* Đây là dòng sẽ hiện lên Google */}
+        <title>Rovian Watch - Thương hiệu đồng hồ số 1 Việt Nam</title>
 
-      {/* LOOKBOOK GRID */}
-      <LookbookGrid />
+        <meta
+          name="description"
+          content="Khám phá bộ sưu tập đồng hồ Rovian cao cấp, tuyệt tác cơ khí với thiết kế sang trọng. Thương hiệu đồng hồ uy tín hàng đầu Việt Nam."
+        />
 
-      <FeaturedProducts />
+        <meta
+          property="og:title"
+          content="Rovian Watch - Thương hiệu đồng hồ số 1 Việt Nam"
+        />
+        <meta
+          property="og:description"
+          content="Tuyệt tác thời gian dành cho người sành điệu."
+        />
+        <meta property="og:image" content="link-anh-thumbnail-cua-ban.jpg" />
+      </Helmet>
+      <div className="min-h-screen bg-black text-white font-['Google_Sans_Flex']">
+        {/* HERO SECTION (LOOKBOOK) */}
+        <HeroSection />
 
-      <AboutSection />
+        {/* LOOKBOOK GRID */}
+        <LookbookGrid />
 
-      <ContactSection />
-    </div>
+        <FeaturedProducts />
+
+        <AboutSection />
+
+        <ContactSection />
+      </div>
+    </>
   );
 };
 
