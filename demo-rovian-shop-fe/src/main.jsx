3d5fc1b26286a5b927331fa43eaf6fa9a4c87cdd
dgeniust <dathiichan141@gmail.com>
2026-04-14 14:40:43 +0700

    fix main.jsx

--- demo-rovian-shop-fe/src/main.jsx
+++ demo-rovian-shop-fe/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async"; // [THÊM MỚI]
 import "./index.css";
 import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </StrictMode>,
-);
+import { ViteReactSSG } from "vite-react-ssg";
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <HelmetProvider>
+//       <App />
+//     </HelmetProvider>
+//   </StrictMode>,
+// );
+export const createApp = ViteReactSSG(<App />);
