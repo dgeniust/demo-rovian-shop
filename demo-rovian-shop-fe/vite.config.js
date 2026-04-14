e9645633976af1af9f8fb103211ac6e327ae32e4
dgeniust <dathiichan141@gmail.com>
2026-04-14 14:38:34 +0700

    fix styled-component

--- demo-rovian-shop-fe/vite.config.js
+++ demo-rovian-shop-fe/vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), ["@swc/plugin-styled-components", {}]],
+  ssr: {
+    noExternal: ["styled-components", "@emotion/*"],
+  },
 });
